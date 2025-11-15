9489875ddda1f0718eb61b2796f95fd2c564c8f6
bastianolea <bastianolea@gmail.com>
2025-11-14 21:14:33 -0300

    datos actualizados

--- otros/faltantes.R
+++ otros/faltantes.R
@@ -38,3 +38,69 @@ Crímenes y simples delitos ley de armas
 # Comercio ilegal
 # Ofensas al pudor
 # Otras incivilidades
+
+
+
+cead_limpiada |> 
+  summarize(sum(as.numeric(cifra)), .by = delitos) |> 
+  print(n=Inf)
+
+# mostrar tabla como html
+codigo_html <- datos_cead[[1]][[1]]
+
+dir <- tempfile()
+dir.create(dir)
+htmlFile <- file.path(dir, "index.html")
+writeLines(codigo_html, con = htmlFile)
+rstudioapi::viewer(htmlFile)
+
+
+# por ser familia o grupos de delitos
+"Delitos contra la vida o integridad de las personas",
+"Homicidios y femicidios",
+"Violaciones y delitos sexuales",
+"Robos con violencia o intimidación", #(repetida)
+"Robos violentos",
+"Delitos contra la propiedad no violentos",
+"Robos en lugares habitados y no habitados",
+"Robos de vehículos y sus accesorios",
+"Incivilidades",
+
+# cubiertos por "Violencia intrafamiliar"
+"Violencia intrafamiliar con lesiones físicas",
+"Violencia intrafamiliar con lesiones psicológicas",
+"Maltrato habitual",
+"Amenazas en contexto de violencia intrafamiliar",
+"Violencia intrafamiliar no clasificada",
+
+# cubiertos por "Delitos asociados a drogas"
+"Tráfico de sustancias",
+"Microtráfico de sustancias",
+"Elaboración o producción de sustancias",
+"Otras infracciones a la ley de drogas",
+
+# cubiertos por "Delitos asociados a armas"
+"Disparo injustificado",
+"Porte / posesión de armas o explosivos",
+"Otras infracciones a la ley de armas",
+
+# cubiertos por "Amenaza falta o riña"
+"Amenaza con armas (falta)",
+"Riña Pública",
+
+# cubiertos por "Consumo de alcohol y drogas en la vía pública"
+"Consumo de drogas en la vía pública",
+"Porte de drogas",
+"Otras faltas a la ley de drogas",
+"Consumo de alcohol en la vía pública",
+
+# cubiertos por "Otras incivilidades"
+"Animales sueltos en la vía pública",
+"Comercio ilegal",
+"Ofensas al pudor",
+"Otras incivilidades",
+
+# cubiertos por "Otros delitos o faltas"
+"Abigeato",
+"Robo frustrado",
+"Otros delitos o faltas" #(repetido)
--- procesar_datos_delincuencia.R
+++ procesar_datos_delincuencia.R
@@ -33,39 +33,91 @@ cead_limpiada <- cead_limpiar_resultados(datos_cead, comunas_por_calcular)
 # filtrar delitos que en la tabla son agrupaciones de delitos individuales, como categorías de delitos que engloban delitos similares,
 # para así dejar solo delitos puntuales, de lo contrario al sumar los delitos habrían conteos repetidos y la cifra sería incorrecta
 cead_limpiada_2 <- cead_limpiada |> 
-  # #sacar categorías que son agrupaciones de delitos individuales
-  # filter(!delitos %in% c("Delitos de mayor connotación social", "Incivilidades",
-  #                        "Violencia intrafamiliar",
-  #                        "Delitos violentos", "Delitos contra la propiedad no violentos",
-  #                        "Delitos asociados a armas", "Otros delitos o faltas",
-  #                        "Homicidios y femicidios", "Violaciones y delitos sexuales",
-  #                        "Crímenes y simples delitos ley de armas", "Robos en lugares habitados y no habitados",
-  #                        "Robos en vehículos y sus accesorios", "Otras incivilidades")) |> 
   #sacar categorías que son agrupaciones de delitos individuales
   filter(!delitos %in% c(
     "Delitos contra la vida o integridad de las personas",
+    "Homicidios y femicidios",
     "Violaciones y delitos sexuales",
+    # "Robos con violencia o intimidación", #(repetida)
     "Robos violentos",
-    "Violencia intrafamiliar ",
-    "Delitos asociados a drogas",
-    "Crímenes y simples delitos ley de armas")) |> 
-  #recodificar homicidios
-  mutate(delitos = recode(delitos,
-                          "Otros homicidios" = "Homicidios"))
+    "Delitos contra la propiedad no violentos",
+    "Robos en lugares habitados y no habitados",
+    "Robos de vehículos y sus accesorios",
+    "Incivilidades")) |> 
+  # saacr delitos menores o irrelevantes cubiertos por agrupaciones
+  filter(!delitos %in% c(
+    # cubiertos por "Violencia intrafamiliar"
+    "Violencia intrafamiliar con lesiones físicas",
+    "Violencia intrafamiliar con lesiones psicológicas",
+    "Maltrato habitual",
+    "Amenazas en contexto de violencia intrafamiliar",
+    "Violencia intrafamiliar no clasificada",
+    
+    # cubiertos por "Delitos asociados a drogas"
+    "Tráfico de sustancias",
+    "Microtráfico de sustancias",
+    "Elaboración o producción de sustancias",
+    "Otras infracciones a la ley de drogas",
+    
+    # cubiertos por "Delitos asociados a armas"
+    "Disparo injustificado",
+    "Porte / posesión de armas o explosivos",
+    "Otras infracciones a la ley de armas",
+    
+    # cubiertos por "Amenaza falta o riña"
+    "Amenaza con armas (falta)",
+    "Riña Pública",
+    
+    # cubiertos por "Consumo de alcohol y drogas en la vía pública"
+    "Consumo de drogas en la vía pública",
+    "Porte de drogas",
+    "Otras faltas a la ley de drogas",
+    "Consumo de alcohol en la vía pública",
+    
+    # cubiertos por "Otras incivilidades"
+    "Animales sueltos en la vía pública",
+    "Comercio ilegal",
+    "Ofensas al pudor",
+    "Otras incivilidades",
+    
+    # cubiertos por "Otros delitos o faltas"
+    "Abigeato",
+    "Robo frustrado"
+    # "Otros delitos o faltas" #(repetido)
+  )) |> 
+  # sacar delitos irrelevantes
+  filter(delitos != "Otros delitos o faltas")
 
-# dentro de los delitos que son agrupaciones de otros delitos, el grupo "Robos con violencia o intimidación" contiene un delito del mismo nombre, por lo que hay que filtrarlo distinto dado que se llaman igual
+# repetidos en grupo y subgrupo:
+# "Robos con violencia o intimidación", #(repetida)
+# "Otros delitos o faltas" #(repetido)
+
+# cead_limpiada_2 |> 
+#   filter(cut_comuna == 1101,
+#          año == 2024, mes == 4) |> 
+#   select(delitos, cifra) |> 
+#   print(n=Inf)
+
+# dentro de los delitos que son agrupaciones de otros delitos, el grupo "Robos con violencia o intimidación" 
+# contiene un delito del mismo nombre, por lo que hay que filtrarlo distinto dado que se llaman igual
 cead_limpiada_3 <- cead_limpiada_2 |> 
+    # filter(cut_comuna == 1101,
+    #        año == 2024, mes == 4) |> 
+    # # select(delitos, cifra) |>
   # "Robos con violencia o intimidación" #sale dos veces, una es agrupación con robo violento de vehiculo motorizado y otra es el real
-  # filter(delitos %in% c("Robos con violencia o intimidación", "Robo violento de vehículo motorizado", "Otros delitos sexuales")) |> #para probar
-  mutate(arreglar = case_when(delitos == "Robos con violencia o intimidación" ~ TRUE,
+  mutate(arreglar = case_when(delitos %in% c("Robos con violencia o intimidación", "Otros delitos o faltas") ~ TRUE,
                               .default = FALSE)) |> 
   # filtrarlos por orden, porque desde la tabla vienen ordenados (primero la suma, luego el dato aislado); es decir, en vez de haber 12 filas del delito (una por mes), hay 24
   group_by(delitos, cut_comuna, año) |> 
   mutate(arreglar_n = ifelse(arreglar == TRUE, 1:n(), NA),
          arreglar_n_total = n()) |> #cantidad de filas, ergo de meses... en el delito repetido son 24, en otros 12 o menos
   ungroup() |> 
-  filter(arreglar == FALSE | (arreglar == TRUE & arreglar_n > arreglar_n_total/2)) |>
+  # dejar solamente el delito, no el grupo
+  filter(arreglar == FALSE | (arreglar == TRUE & arreglar_n == 2)) |>
   select(-arreglar, -arreglar_n, -arreglar_n_total)
+
+n_distinct(cead_limpiada_4$delito)
+
 
 # crear fecha, corregir formatos de columnas
 cead_limpiada_4 <- cead_limpiada_3 |> 
@@ -78,11 +130,8 @@ cead_limpiada_4 <- cead_limpiada_3 |>
   mutate(across(where(is.character), as.factor)) |> 
   select(comuna, cut_comuna, region, cut_region, fecha, delito, delito_n, everything())
 
-max(cead_limpiada_4$fecha)
 
-cead_limpiada_4 |> 
-  group_by(fecha) |> 
-  summarize(sum(delito_n))
+max(cead_limpiada_4$fecha)
 
 # cortar fecha de corte de la base de datos manualmente, porque cead reporta 0 delitos en meses donde no tiene datos, o sea que si la base llega hasta marzo de 2024, abril 2024 muestra 0
 cead_limpiada_5 <- cead_limpiada_4 |> 
@@ -92,13 +141,14 @@ cead_limpiada_5 <- cead_limpiada_4 |>
 
 # revisar
 waldo::compare(cead_limpiada_4, cead_limpiada_5)
-visdat::vis_miss(cead_limpiada_5)
+# visdat::vis_miss(cead_limpiada_5, warn_large_data = F)
 
 # cead_limpiada_4 |>
 #   filter(fecha > "2024-09-01") |>
 #   summarize(max(delito_n))
 
 # cead |> count(delito)
+
 
 
 # unir con datos anteriores (opcional) ----
@@ -131,9 +181,12 @@ waldo::compare(cead_anterior |> pull(delito) |> unique() |> sort() |> as.charact
 cead_unido <- cead_anterior |>
   bind_rows(cead_nuevo) |>
   arrange(fecha, comuna, delito)
-# hasta aquí
+# (hasta aquí el proceso de actualización de base anterior)
 
+# (si no se actualiza base anterior, ejecutar lo siguiente)
 # cead_unido <- cead_limpiada_5
+
+
 
 # revisar ----
 
@@ -183,5 +236,6 @@ arrow::write_parquet(cead_unido, "app/cead_delincuencia.parquet")
 
 # para usuarios
 arrow::write_parquet(cead_unido, "datos_procesados/cead_delincuencia_chile.parquet")
+readr::write_rds(cead_unido, "datos_procesados/cead_delincuencia_chile.rds", compress = "gz")
 # readr::write_csv2(cead_unido, "datos_procesados/cead_delincuencia_chile.csv")
 
